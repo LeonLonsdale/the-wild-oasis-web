403f5bfc7a5bddb163da287fa4ff2adc0fbc0fb2
LeonLonsdale <leon.coding@icloud.com>
2024-08-11 21:24:46 +0100

    cleanup path helper types

--- src/_lib/paths.tsx
+++ src/_lib/paths.tsx
@@ -3,59 +3,47 @@ import {
   HomeIcon,
   UserIcon,
 } from "@heroicons/react/24/solid";
-import { DatabaseId } from "./types";
 
 export type PathFn = (...args: PathFnArg[]) => string;
-export type PathFnArg = string | number | DatabaseId;
+export type PathFnArg = string | number;
 
-export type NavLink = {
+export type PathItem = {
   label: string;
   path: PathFn;
   icon?: React.ReactNode;
 };
-export type Paths = {
-  [key: string]: {
-    navs: string[];
-  } & NavLink;
-};
+export type Paths = Record<string, PathItem>;
 
 const paths: Paths = {
   home: {
     label: "Home",
     path: () => "/",
-    navs: ["mainNav"],
   },
   about: {
     label: "About",
     path: () => "/about",
-    navs: ["mainNav"],
   },
   cabins: {
     label: "Cabins",
     path: () => "/cabins",
-    navs: ["mainNav"],
   },
   viewCabin: {
     label: "View Cabin",
-    path: (...args: (string | number | DatabaseId)[]) => `/cabins/${args[0]}`,
-    navs: ["cabinsNav"],
+    path: (...args: (string | number)[]) => `/cabins/${args[0]}`,
   },
   account: {
     label: "My Account",
     path: () => "/account",
-    navs: ["mainNav"],
     icon: <HomeIcon className="h-5 w-5 text-primary-600" />,
   },
   accountReservations: {
     label: "Reservations",
     path: () => "/account/reservations",
-    navs: ["accountNav"],
     icon: <CalendarDaysIcon className="h-5 w-5 text-primary-600" />,
   },
   accountProfile: {
     label: "Profile",
     path: () => "/account/profile",
-    navs: ["accountNav"],
     icon: <UserIcon className="h-5 w-5 text-primary-600" />,
   },
 };
@@ -63,5 +51,5 @@ const paths: Paths = {
 export const getPath = (key: string, ...args: PathFnArg[]) =>
   args.length ? paths[key].path(...args) : paths[key].path();
 
-export const makeNav = (keys: string[]): NavLink[] =>
+export const makeNav = (keys: string[]): PathItem[] =>
   keys.map((key) => paths[key] ?? null).filter((navLink) => navLink !== null);
